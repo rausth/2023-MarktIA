42eb337151e565f3537aeab70cdec7e44038b42f
enzocussuol <enzo@allsystem.com.br>
2023-07-18 12:05:39 -0300

    :arrow_up: Adicionando Jena para realizar consultas SPARQL no dbpedia

--- marktia-backend/src/main/java/ufes/marktiabackend/controllers/FederationController.java
+++ marktia-backend/src/main/java/ufes/marktiabackend/controllers/FederationController.java
@@ -1,6 +1,10 @@
 package ufes.marktiabackend.controllers;
 
 import lombok.RequiredArgsConstructor;
+import org.apache.jena.query.QueryExecution;
+import org.apache.jena.query.QuerySolution;
+import org.apache.jena.query.ResultSet;
+import org.apache.jena.rdf.model.Literal;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ufes.marktiabackend.dtos.responses.federation.FederationFieldResponseDTO;
@@ -16,7 +20,37 @@ public class FederationController {
 
     @GetMapping("/states")
     public ResponseEntity<List<FederationFieldResponseDTO>> getStates() {
-        return ResponseEntity.ok(federationService.getStates());
+//        return ResponseEntity.ok(federationService.getStates());
+
+        /**
+         * Consulta SPARQL
+         */
+        String query = "PREFIX dbo: <http://dbpedia.org/ontology/>\n"
+                + "PREFIX dbp: <http://dbpedia.org/property/>\n"
+                + "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n"
+                + "select ?desc\n"
+                + "where {\n"
+                + "    ?uri a dbo:City ;\n"
+                + "        dbp:name \"Rio de Janeiro\"@en ;\n"
+                + "        rdfs:comment ?desc .\n"
+                + "FILTER(langMatches(lang(?desc), \"EN\"))\n"
+                + "}";
+
+        QueryExecution queryExecution = QueryExecution
+                .service("https://dbpedia.org/sparql")
+                .query(query)
+                .build();
+
+        ResultSet results = queryExecution.execSelect();
+
+        if (results.hasNext()) {
+            QuerySolution querySolution = results.next();
+            Literal literal = querySolution.getLiteral("desc");
+
+            System.out.println(literal.getValue());
+        }
+
+        return ResponseEntity.ok(null);
     }
 
     @GetMapping("/regions")
